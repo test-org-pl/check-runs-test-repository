3aa33cb28e0102225e8a38325741add9d7a3357e
Eugène Nélou <eugene.nelou@gmail.com>
2020-01-17 16:59:07 +0100

    undo leak DO

--- test.rb
+++ test.rb
@@ -3,24 +3,3 @@ puts "testing gitignore"
 
 MY_SLACK_KEY="xoxb-263594206564-FGqddMF8t08v8N7Oq4i57vs1"
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-DIGITALOCEAN_ACCESS_TOKEN=9248f2a0b21707d1b42386867093f7e764e79c92791fdbdb85bd342634fdefce
